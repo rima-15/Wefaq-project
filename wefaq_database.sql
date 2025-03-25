d0d15ebdf0fed734d328d1eb3524f91a26acfd4f
rima-15 <161233187+rima-15@users.noreply.github.com>
2025-03-25 22:33:53 +0300

    Add files via upload

---
+++ wefaq_database.sql
@@ -0,0 +1,395 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 25, 2025 at 07:32 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wefaq_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `chat_ID` int(11) NOT NULL,
+  `project_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file`
+--
+
+CREATE TABLE `file` (
+  `file_ID` int(11) NOT NULL,
+  `file_name` varchar(40) NOT NULL,
+  `file_type` varchar(15) NOT NULL,
+  `file_size` double NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `uploaded_by` int(11) NOT NULL,
+  `project_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invite`
+--
+
+CREATE TABLE `invite` (
+  `inviter_ID` int(11) NOT NULL,
+  `invitee_ID` int(11) NOT NULL,
+  `project_ID` int(11) NOT NULL,
+  `status` enum('pending','accepted','declined') NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `message_ID` int(11) NOT NULL,
+  `message_text` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sender_ID` int(11) DEFAULT NULL,
+  `chat_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `notification_ID` int(11) NOT NULL,
+  `type` enum('invite','rate') NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `status` enum('Unread','read') NOT NULL DEFAULT 'Unread',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_ID` int(11) NOT NULL,
+  `related_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `project_ID` int(11) NOT NULL,
+  `project_name` varchar(40) NOT NULL,
+  `project_description` varchar(255) NOT NULL,
+  `project_deadline` date NOT NULL,
+  `status` enum('in progress','completed') NOT NULL DEFAULT 'in progress',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `leader_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projectteam`
+--
+
+CREATE TABLE `projectteam` (
+  `user_ID` int(11) NOT NULL,
+  `project_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rate`
+--
+
+CREATE TABLE `rate` (
+  `rater_ID` int(11) NOT NULL,
+  `rated_ID` int(11) NOT NULL,
+  `rating_value` int(11) NOT NULL,
+  `status` enum('Pending','Done') NOT NULL,
+  `skill_ID` int(11) NOT NULL,
+  `related_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skill`
+--
+
+CREATE TABLE `skill` (
+  `skill_ID` int(11) NOT NULL,
+  `skill_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `task_ID` int(11) NOT NULL,
+  `task_name` varchar(40) NOT NULL,
+  `task_description` varchar(255) NOT NULL,
+  `status` enum('unassigned','not started','in progress','completed') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `task_deadline` date NOT NULL,
+  `assigned_to` int(11) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `project_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_ID` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `user_type` varchar(15) NOT NULL,
+  `organization` varchar(40) NOT NULL,
+  `bio` varchar(80) DEFAULT NULL,
+  `email` varchar(40) NOT NULL,
+  `phone_num` varchar(10) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chat_ID`),
+  ADD KEY `project_ID` (`project_ID`);
+
+--
+-- Indexes for table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`file_ID`),
+  ADD KEY `uploaded_by` (`uploaded_by`),
+  ADD KEY `project_ID` (`project_ID`);
+
+--
+-- Indexes for table `invite`
+--
+ALTER TABLE `invite`
+  ADD KEY `inviter_ID` (`inviter_ID`),
+  ADD KEY `invitee_ID` (`invitee_ID`),
+  ADD KEY `project_ID` (`project_ID`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`message_ID`),
+  ADD KEY `sender_ID` (`sender_ID`),
+  ADD KEY `chat_ID` (`chat_ID`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`notification_ID`),
+  ADD KEY `user_ID` (`user_ID`),
+  ADD KEY `related_ID` (`related_ID`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`project_ID`),
+  ADD KEY `leader_ID` (`leader_ID`);
+
+--
+-- Indexes for table `projectteam`
+--
+ALTER TABLE `projectteam`
+  ADD KEY `user_ID` (`user_ID`),
+  ADD KEY `project_ID` (`project_ID`);
+
+--
+-- Indexes for table `rate`
+--
+ALTER TABLE `rate`
+  ADD KEY `rater_ID` (`rater_ID`),
+  ADD KEY `rated_ID` (`rated_ID`),
+  ADD KEY `skill_ID` (`skill_ID`),
+  ADD KEY `related_ID` (`related_ID`);
+
+--
+-- Indexes for table `skill`
+--
+ALTER TABLE `skill`
+  ADD PRIMARY KEY (`skill_ID`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`task_ID`),
+  ADD KEY `project_ID` (`project_ID`),
+  ADD KEY `assigned_to` (`assigned_to`),
+  ADD KEY `created_by` (`created_by`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chat_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `file`
+--
+ALTER TABLE `file`
+  MODIFY `file_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `message_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `notification_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `project_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `skill`
+--
+ALTER TABLE `skill`
+  MODIFY `skill_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `task_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chat`
+--
+ALTER TABLE `chat`
+  ADD CONSTRAINT `chat_project` FOREIGN KEY (`project_ID`) REFERENCES `project` (`project_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `file`
+--
+ALTER TABLE `file`
+  ADD CONSTRAINT `file_create` FOREIGN KEY (`uploaded_by`) REFERENCES `projectteam` (`user_ID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `file_project` FOREIGN KEY (`project_ID`) REFERENCES `projectteam` (`project_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `invite`
+--
+ALTER TABLE `invite`
+  ADD CONSTRAINT `invitee` FOREIGN KEY (`invitee_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `inviter_user` FOREIGN KEY (`inviter_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `project_project` FOREIGN KEY (`project_ID`) REFERENCES `project` (`project_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_chat` FOREIGN KEY (`chat_ID`) REFERENCES `chat` (`chat_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `message_sender` FOREIGN KEY (`sender_ID`) REFERENCES `user` (`user_ID`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `notification`
+--
+ALTER TABLE `notification`
+  ADD CONSTRAINT `noti_project` FOREIGN KEY (`related_ID`) REFERENCES `project` (`project_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `noti_user` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `project`
+--
+ALTER TABLE `project`
+  ADD CONSTRAINT `leader` FOREIGN KEY (`leader_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `projectteam`
+--
+ALTER TABLE `projectteam`
+  ADD CONSTRAINT `project` FOREIGN KEY (`project_ID`) REFERENCES `project` (`project_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user` FOREIGN KEY (`user_ID`) REFERENCES `user` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `rate`
+--
+ALTER TABLE `rate`
+  ADD CONSTRAINT `rated_user` FOREIGN KEY (`rated_ID`) REFERENCES `user` (`user_ID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `rater_user` FOREIGN KEY (`rater_ID`) REFERENCES `user` (`user_ID`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `related_project` FOREIGN KEY (`related_ID`) REFERENCES `project` (`project_ID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `skill_skill` FOREIGN KEY (`skill_ID`) REFERENCES `skill` (`skill_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `task-project` FOREIGN KEY (`project_ID`) REFERENCES `project` (`project_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `task_assign` FOREIGN KEY (`assigned_to`) REFERENCES `projectteam` (`user_ID`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `task_create` FOREIGN KEY (`created_by`) REFERENCES `projectteam` (`user_ID`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
