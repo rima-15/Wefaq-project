f3b351b2db9680da0024f2c9ba9514545b323575
noorge <155255184+noorge@users.noreply.github.com>
2025-03-26 05:18:31 +0300

    Update connection.php

--- wefaq-php/connection.php
+++ wefaq-php/connection.php
@@ -5,9 +5,6 @@ $password = "root";
 $database = "wefaq_database";
 
 $conn = mysqli_connect($host, $username, $password, $database);
-if($conn){
-    echo "<script>alert('Connected successfully')</script>";
-}
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
