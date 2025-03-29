4a92c032d920871b8887dcbc781983c4a412a9a3
Mona <monaalnajjar2002@gmail.com>
2025-03-29 07:08:02 +0300

    Merge origin/temp
    Conflicts:
    	public_html/AppointmentPage.html
    	public_html/PatientHome.html
    	public_html/PrescribePage.html
    	public_html/style.css

--- wefaq-php/login.php
+++ wefaq-php/login.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $_SESSION['user_type'] = $user['user_type'];
                 
                 // Redirect to dashboard
-                header("Location: chatroom.php");
+                header("Location: dashboard.php");
                 exit();
             } else {
                 $error = "Invalid username/email or password.";
